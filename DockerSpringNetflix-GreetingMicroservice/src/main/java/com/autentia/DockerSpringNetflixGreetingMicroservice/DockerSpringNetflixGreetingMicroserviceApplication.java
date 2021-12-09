00b3074ad6b334839644f3e7c9297e248a6c2f52
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-12-09 14:05:50 -0300

    microservice: microservice created

---
+++ DockerSpringNetflix-GreetingMicroservice/src/main/java/com/autentia/DockerSpringNetflixGreetingMicroservice/DockerSpringNetflixGreetingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.autentia.DockerSpringNetflixGreetingMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringNetflixGreetingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringNetflixGreetingMicroserviceApplication.class, args);
+	}
+
+}
